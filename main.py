2bda1f1ebfbea44b335bf6387c47fa478aed47c2
Jaime Travesedo <jaimetravesedo125@gmail.com>
2022-04-15 19:35:37 +0200

    Created distributable package

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-from chipy8.run import run
-
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
-if __name__ == "__main__":
-    sys.exit(run("roms/test_opcode.ch8"))
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CHIPy8",
+    version="0.0.1",
+    author="Jaime Travesedo",
+    author_email="jaimetravesedo125@gmail.com",
+    description="CHIP8 interpreter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jtl125/CHIPy-8",
+    project_urls={
+        "Bug Tracker": "https://github.com/jtl125/CHIPy-8/issues",
+    },
+    scripts=["bin/chipy8"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pygame',
+    ],
+    packages=['chipy8'],
+    python_requires='>=3.8',
+)
