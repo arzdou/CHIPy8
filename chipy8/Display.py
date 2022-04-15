ddd925c24cc1abe3d66dcc9d10292ad5f5307eea
Jaime Travesedo <jaimetravesedo125@gmail.com>
2022-04-15 16:26:54 +0200

    Added keyboard input support

--- chipy8/Display.py
+++ chipy8/Display.py
@@ -4,6 +4,25 @@ BACKGROUND = (0,0,0)
 COLOR = (255,255,255)
 UPSCALE = 10
 
+KEY_MAP = {
+    0x0: pygame.K_g,
+    0x1: pygame.K_4,
+    0x2: pygame.K_5,
+    0x3: pygame.K_6,
+    0x4: pygame.K_7,
+    0x5: pygame.K_r,
+    0x6: pygame.K_t,
+    0x7: pygame.K_y,
+    0x8: pygame.K_u,
+    0x9: pygame.K_f,
+    0xA: pygame.K_h,
+    0xB: pygame.K_j,
+    0xC: pygame.K_v,
+    0xD: pygame.K_b,
+    0xE: pygame.K_n,
+    0xF: pygame.K_m,
+}
+
 class Display():
     def __init__(self):
         # Display
@@ -45,6 +64,11 @@ class Display():
         if color == BACKGROUND:
             pixel = False
         return pixel
+
+    def is_key_pressed(self, key_index):
+        key = KEY_MAP[key_index]
+        pressed_keys = pygame.key.get_pressed()
+        return pressed_keys[key]
     
     def clear(self):
         self.screen.fill(BACKGROUND)
